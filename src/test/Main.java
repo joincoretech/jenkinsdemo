c1b05c3a4773c6529cef0a39637367c8e4921150
azizullahayoubi <azizullahayoubi19@gmail.com>
2023-06-24 14:18:16 -0400

    first commit

---
+++ src/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i=0; i<5; i++){
+            System.out.println("this is our first jenkins code, Hello Batch 1");
+
+        }
+    }
+}
